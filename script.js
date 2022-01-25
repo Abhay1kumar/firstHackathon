0a0d2e9f629c8f93c52121861ebf518a91b39c57
Abhay1kumar <kumarabhay401@gmail.com>
2022-01-25 12:51:32 +0530

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,81 @@
-document.body.innerHTML = `<label>Search Box</label>
-<input type="search" id="search" placeholder="Search here">
-<div><button type="button" id="btn">Click Here</button></div>
-<div class="catdata"></div>`;
+document.body.innerHTML = `
+<div> <button id="btn">Click Here</button>
+<input type="text" id="search">
+</div>
+
+<div class="popup" >
+<div class="dataDiv">
+<h2 class="heading"></h2>
+<div class="img"></div>
+<div class="close-btn"><button>Close</button></div>
+</div>
+</div>
+
+<div class="catdata"></div>
+`;
+
 
 let button = document.querySelector('#btn');
-button.addEventListener("click", function () {
-    let takeData = document.querySelector('#search').value;
+let popup=document.querySelector('.popup');
+let closebtn=document.querySelector('.close-btn');
+closebtn.addEventListener('click', () =>{
+    popup.classList.remove('active');
+})
 
-    console.log(takeData);
+button.addEventListener("click", function () {
+popup.classList.add("active")
+    var takeData = document.querySelector('#search').value;
+let heading= document.querySelector('.heading');
+heading.innerHTML = takeData;
+
+    // console.log(takeData);
+
+
 });
 
+
+// get the the search api data
+
+async function getSearchdata(){
+    let searchdata;
+    try {
+        const data = await fetch("https://cataas.com/api/cats?tags=cute",{
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        searchdata = await data.json();
+        // console.log(searchdata);
+        var catname;
+        
+        searchdata.forEach(tag => {
+            catname = tag.tags;
+            // console.log(catname);
+        })     
+        
+    } catch (e) {
+        console.log(e);
+    }
+   return catname;
+}
+getSearchdata();
+
+let imageclass=document.querySelector('.img')
+
+async function DisplaySearchdata(){
+    let catname=await getSearchdata();
+    // console.log(catname);
+
+    if(catname==search){
+        return ``
+    }
+    }
+    DisplaySearchdata();
+
+  
+// ********************************************************************************************
+// List all the cats on the screen with a given api https://cataas.com/api/cats.
 
 // get the cat api data
 
@@ -26,7 +92,7 @@ async function getApidata() {
             }
         );
         catapi = await data.json();
-        // console.log(catapi);
+        console.log(catapi);
 
     } catch (err) {
         console.log(err);
